fb400e655e4d131db58f2559903b98d412f48981
pseudozach <git@pseudozach.com>
2022-03-12 02:20:13 +0100

    add alert

--- src/components/swaptab/newdesktopswaptab.js
+++ src/components/swaptab/newdesktopswaptab.js
@@ -42,6 +42,8 @@ import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import Button from '@mui/material/Button';
 import SwapVertIcon from '@mui/icons-material/SwapVert';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import Alert from '@mui/material/Alert';
+import Box from '@mui/material/Box';
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -139,7 +141,7 @@ const DeskTopSwapTabContent = ({
 
           <TextField
             variant="outlined"
-            disable={disabled}
+            // disable={disabled}
             className={classes.inputMobile}
             min={0}
             max={maxAmount}
@@ -147,6 +149,7 @@ const DeskTopSwapTabContent = ({
             error={inputError}
             value={baseAmount}
             onChange={updateQuoteAmount}
+            type="number"
           />
 
         </Paper>
@@ -155,6 +158,7 @@ const DeskTopSwapTabContent = ({
           <IconButton 
             aria-label="Swap Pairs" 
             className={classes.flipbutton}
+            onClick={switchPair} 
           >
             <ArrowDownwardIcon />
           </IconButton>
@@ -193,7 +197,7 @@ const DeskTopSwapTabContent = ({
 
           <TextField
             variant="outlined"
-            disable={disabled}
+            // disable={disabled}
             className={classes.inputMobile}
             min={baseStep}
             max={Number.MAX_SAFE_INTEGER}
@@ -201,6 +205,7 @@ const DeskTopSwapTabContent = ({
             error={inputError}
             value={quoteAmount}
             onChange={updateBaseAmount}
+            type="number"
           />
 
         </Paper>
@@ -236,61 +241,24 @@ const DeskTopSwapTabContent = ({
         </Typography> */}
       </CardContent>
       <CardActions
-        sx={{justifyContent: 'flex-end',}}>
-        <Button variant="contained" size="large" endIcon={<NavigateNextIcon />}
-          sx={{ margin: 2, }}
-          className={classes.greenman}
+        sx={{flexDirection: 'column', }}
+      >
+        <Box
+          sx={{display: 'flex', justifyContent: 'flex-end', width: '100%', alignItems: 'center', }}
         >
-          Start
-        </Button>
-        {/* <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore> */}
+          {(error || inputError) && <Alert severity="error" sx={{flex: 1, mx: 1,}} >{errorMessage}</Alert>}
+          <Button variant="contained" size="large" endIcon={<NavigateNextIcon />}
+            sx={{ margin: 2, }}
+            className={classes.greenman}
+            disabled={(error || inputError)}
+            onClick={()=>shouldSubmit()}
+          >
+            Start
+          </Button>
+        </Box>
+        
       </CardActions>
-
-
-
-
-      {/* <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-        </CardContent>
-      </Collapse> */}
+      
     </Card>
 
 
